1dddbf5fab6feacdea6b7ffcd8a68359b79e264a
VinKira <vincentiussutanto88@gmail.com>
2026-08-27 20:17:12 +0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,1 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
 
+const supabaseUrl = 'https://mjxkpyasxjnfgzttvuvy.supabase.co'
+const supabaseAnonKey = 'sb_publishable_ha9xlTJ6_0CaBAQGoH_szA_X8IzSFdu'
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
