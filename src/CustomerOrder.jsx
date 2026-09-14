b43053fac8e498af58f92163ff0f0041bc926c2d
VinKira <vincentiussutanto88@gmail.com>
2026-09-14 18:02:14 +0700

    Decrease font size of brand tagline from 16px to 10px

--- src/CustomerOrder.jsx
+++ src/CustomerOrder.jsx
@@ -384,7 +384,7 @@ logoImage: {
     lineHeight: '1.1',
   },
   brandTagline: {
-    fontSize: '16px',
+    fontSize: '10px',
     letterSpacing: '2px',
     color: '#d4af37',
     display: 'block',
